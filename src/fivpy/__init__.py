d1203aaa932c1f5f3e85ca3639f6dc93e6e03f7c
TheilonMacedo <theilonhenrique@gmail.com>
2022-01-14 01:40:22 -0300

    refactor: updatig module invoking

--- src/fivpy/__init__.py
+++ src/fivpy/__init__.py
@@ -1,3 +1,8 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("fivpy")
+
+from fivpy.srs import RandomSample
+from fivpy.datasets import data_1
+from fivpy.datasets import data_2
+from fivpy.datasets import data_3
